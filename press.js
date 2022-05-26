f38fe1dc3e60eea3d264c364fb132d4ac368cb8f
julesss-coder <julesss@posteo.de>
2022-05-26 14:37:01 +0200

    Added page template files

---
+++ press.js
@@ -0,0 +1,14 @@
+// press.js hosts the code for the press.html page only
+
+const App = () => {
+  return (
+    <Template>
+      <h1>Press page</h1>
+    </Template>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js hosts the React application code for index.html file.
+const App = () => {
+  return (
+    <Template>
+      <h1>Main content</h1>
+    </Template>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
